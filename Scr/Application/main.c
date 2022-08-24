01e7c0257b8a8059d19c14c4c24b7704ef28654e
Aliaa <82209139+Aliaaahmed333@users.noreply.github.com>
2022-08-24 22:41:58 +0200

    Update main.c

--- Scr/Application/main.c
+++ Scr/Application/main.c
@@ -1,10 +1,20 @@
-#include "intCtrl.h"
+#include "std_Types.h"
+#include "GPIO_Dio.h"
+#include "GPIOPort.h"
+#include "SysTic_Ctrl.h"
 
 int main(void)
 {
-	IntCtrl_Init();
+	
+	Port_Init();
+	DIO_WriteChannel(Dio_Port_A,Dio_PIN0, STD_high);
 	while(1)
 	{
+		
 	}
+	
+	
+	
+	
 	return 0;
 }
