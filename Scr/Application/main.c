6368556a0e8504864adae1b8d34baf8df622f735
Aliaa <82209139+Aliaaahmed333@users.noreply.github.com>
2022-08-26 10:52:25 +0200

    Update main.c

--- Scr/Application/main.c
+++ Scr/Application/main.c
@@ -1,20 +1,45 @@
+/***********************************************************************************************
+* INCLUDES
+***********************************************************************************************/
 #include "std_Types.h"
+#include "BIT_MATH.h"
 #include "GPIO_Dio.h"
 #include "GPIOPort.h"
 #include "SysTic_Ctrl.h"
 
+volatile void (*ptr)(void) =  NULL;
+/********************************************************************************
+* LOCAL FUNCTION PROTOTYPES
+********************************************************************************/
+void BlinkLed (void);
+void CallBackFun(void (*ptr_call)(void));
+
 int main(void)
 {
-	
+
 	Port_Init();
-	DIO_WriteChannel(Dio_Port_A,Dio_PIN0, STD_high);
+	CallBackFun(  BlinkLed );
 	while(1)
 	{
 		
 	}
 	
-	
-	
-	
 	return 0;
+}
+
+void BlinkLed (void)
+{
+	DIO_WriteChannel(Dio_Port_A,Dio_PIN0, STD_high);
+	SysTic_Start(2);
+	DIO_WriteChannel(Dio_Port_A,Dio_PIN0, STD_low);
+	SysTic_Start(3);
+}
+void CallBackFun(void (*ptr_call)(void))
+{
+	ptr = ptr_call;
+}
+
+void SysTick_Handler(void)
+{
+	(*ptr)();
 }
